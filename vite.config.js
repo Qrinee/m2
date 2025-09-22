237057ab66222b47a2f5120f4d617ff89697c8eb
Mona Lisa <kajman5021@gmail.com>
2025-09-22 14:28:04 +0200

    asd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['m2notarialnie-frontend-eatobk-c2d579-51-75-160-15.traefik.me']
+  }
 })
