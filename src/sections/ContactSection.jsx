010758427091a53e07112cfa330572d1817c93fe
Mona Lisa <kajman5021@gmail.com>
2025-10-07 13:36:02 +0200

    asdfasdf

--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -35,7 +35,7 @@ export default function ContactSection() {
     setMessage('')
 
     try {
-      const response = await fetch(`${backend}/inquiry/contact`, {
+      const response = await fetch(`${backend}/emails/contact`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
