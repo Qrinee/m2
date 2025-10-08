92daa4e20374ec584039e6b2df379cf9f87a96b2
Mona Lisa <kajman5021@gmail.com>
2025-10-08 16:34:46 +0200

    asdasd

--- src/sections/TeamSection.jsx
+++ src/sections/TeamSection.jsx
@@ -12,7 +12,7 @@ export default function TeamSection() {
         <p  className='p'>Jesteśmy dumni z jakości usług, które świadczymy. Nasz zespół ekspertów pomoże Ci znaleźć idealną nieruchomość lub szybko sprzedać Twoją.</p>
        </div>
        <div className='card-section'>
-        <Link to={'/estate_agent/68e017c4503da4512138401e'} style={{textDecoration: 'none', color: 'inherit'}}>
+        <Link to={'/estate_agent/68e66f7258e3141c145e3d5d'} style={{textDecoration: 'none', color: 'inherit'}}>
         <PhotoCard 
         image={dawid}
         name={'Dawid Frey'}
@@ -20,18 +20,23 @@ export default function TeamSection() {
         description={'Współtwórca M2Notarialnie i specjalista w zakresie analiz rynku oraz procesu sprzedaży i z ...'}
         />
         </Link>
+
+         <Link to={'/estate_agent/68e66abc58e3141c145e3d19'} style={{textDecoration: 'none', color: 'inherit'}}>
         <PhotoCard
         image={wiktoria}
         name={'Wiktoria Kisio'}
         status={'Członek zarządu | CEO'}
         description={'Współzałożycielka M2 Notarialnie, z doświadczeniem w branży nieruchomości i pasją do tworz ...'}
         />
+        </Link>
+         <Link to={'/estate_agent/68e66e6f58e3141c145e3d4c'} style={{textDecoration: 'none', color: 'inherit'}}>
         <PhotoCard
         image={renata}
         name={'Renata Szwałek'}
         status={'Przedstawiciel'}
         description={'Renata Szwałek to negocjator rynku nieruchomości z wieloletnim doświadczeniem w branży fin ...'}
         />
+        </Link>
         </div>
         </div>
   )
