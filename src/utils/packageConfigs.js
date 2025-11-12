3c4b64f985354ab5a38c9813befeb0a95b383e33
Mona Lisa <kajman5021@gmail.com>
2025-11-12 23:44:35 +0100

    dsa

---
+++ src/utils/packageConfigs.js
@@ -0,0 +1,212 @@
+export const PACKAGE_CONFIGS = {
+  d126: [
+    {
+      category: 'formalities',
+      title: "Pakiet Formalności zgłoszenia i pozwolenia na budowę",
+      vat: "VAT 23%",
+      options: [
+        { id: 'formal-1', name: "Zgłoszenie Budynku Rekreacji Indywidualnej", price: 4420 },
+        { id: 'formal-2', name: "Zgłoszenie budynku Mieszkalnego lub Pozwolenie na budowę", price: 15640 },
+        { id: 'formal-3', name: "Formalność wykonam we własnym zakresie, kupuję tylko projekt", price: 8113 }
+      ]
+    },
+    {
+      category: 'foundation',
+      title: "Fundamenty",
+      options: [
+        { id: 'foundation', name: "Ocieplona, zbrojona płyta fundamentowa", price: 116000 }
+      ]
+    },
+    {
+      category: 'utilities',
+      title: "Instalacje Sanitarne i Elektryczne",
+      options: [
+        { id: 'utilities', name: "Pakiet Instalacje Sanitarne i Elektryczne", price: 76000 }
+      ]
+    },
+    {
+      category: 'roofing',
+      title: "Ściany Wewnętrzne + Obróbki Blacharskie",
+      options: [
+        { id: 'roofing', name: "Pakiet Obróbki Blacharskie, Ocieplenie Sufitu, Płyty G-K Sufit", price: 86500 }
+      ]
+    },
+    {
+      category: 'heating',
+      title: "Ogrzewanie Budynku",
+      options: [
+        { id: 'heating-1', name: "Folie grzewcze + podłogówka (styropian, paroizolacja, ogrzewanie podłogowe, rozdzielacze, wylewka, CO, CWU)", price: 0, customPrice: true },
+        { id: 'heating-2', name: "Kocioł na paliwo stałe (pelet) + komin + podłogówka", price: 0, customPrice: true },
+        { id: 'heating-3', name: "Pompa ciepła powietrze-woda + podłogówka", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'sanitary',
+      title: "Pakiet Sanitarny",
+      options: [
+        { id: 'sanitary', name: "Szambo 10 m³ lub oczyszczalnia", price: 16500 }
+      ]
+    },
+    {
+      category: 'photovoltaic',
+      title: "Fotowoltaika",
+      options: [
+        { id: 'photovoltaic', name: "Indywidualna wycena uzależniona od mocy systemu", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'terrace',
+      title: "Taras i Oświetlenie",
+      options: [
+        { id: 'terrace', name: "Pakiet Taras i Oświetlenie Zewnętrzne - indywidualna wycena", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'turnkey',
+      title: "Wykończenie pod klucz",
+      options: [
+        { id: 'turnkey', name: "Kompletne wykończenie pod klucz - indywidualne kalkulacje", price: 0, customPrice: true }
+      ]
+    }
+  ],
+  d70: [
+    {
+      category: 'formalities',
+      title: "Pakiet Formalności zgłoszenia i pozwolenia na budowę",
+      vat: "VAT 23%",
+      options: [
+        { id: 'formal-1', name: "Zgłoszenie Budynku Rekreacji Indywidualnej", price: 4420 },
+        { id: 'formal-2', name: "Zgłoszenie budynku Mieszkalnego lub Pozwolenie na budowę", price: 15640 },
+        { id: 'formal-3', name: "Formalność wykonam we własnym zakresie, kupuję tylko projekt", price: 8113 }
+      ]
+    },
+    {
+      category: 'foundation',
+      title: "Fundamenty",
+      options: [
+        { id: 'foundation', name: "Ocieplona, zbrojona płyta fundamentowa", price: 78600 }
+      ]
+    },
+    {
+      category: 'utilities',
+      title: "Instalacje Sanitarne i Elektryczne",
+      options: [
+        { id: 'utilities', name: "Pakiet Instalacje Sanitarne i Elektryczne", price: 46200 }
+      ]
+    },
+    {
+      category: 'roofing',
+      title: "Ściany Wewnętrzne + Obróbki Blacharskie",
+      options: [
+        { id: 'roofing', name: "Pakiet Obróbki Blacharskie, Ocieplenie Sufitu, Płyty G-K Sufit", price: 74100 }
+      ]
+    },
+    {
+      category: 'heating',
+      title: "Ogrzewanie Budynku",
+      options: [
+        { id: 'heating-1', name: "Folie grzewcze + podłogówka (styropian, paroizolacja, ogrzewanie podłogowe, rozdzielacze, wylewka, CO, CWU)", price: 0, customPrice: true },
+        { id: 'heating-2', name: "Kocioł na paliwo stałe (pelet) + komin + podłogówka", price: 0, customPrice: true },
+        { id: 'heating-3', name: "Pompa ciepła powietrze-woda + podłogówka", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'sanitary',
+      title: "Pakiet Sanitarny",
+      options: [
+        { id: 'sanitary', name: "Szambo 10 m³ lub oczyszczalnia", price: 16500 }
+      ]
+    },
+    {
+      category: 'photovoltaic',
+      title: "Fotowoltaika",
+      options: [
+        { id: 'photovoltaic', name: "Indywidualna wycena uzależniona od mocy systemu", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'terrace',
+      title: "Taras i Oświetlenie",
+      options: [
+        { id: 'terrace', name: "Pakiet Taras i Oświetlenie Zewnętrzne - indywidualna wycena", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'turnkey',
+      title: "Wykończenie pod klucz",
+      options: [
+        { id: 'turnkey', name: "Kompletne wykończenie pod klucz - indywidualne kalkulacje", price: 0, customPrice: true }
+      ]
+    }
+  ],
+  d115: [
+    {
+      category: 'formalities',
+      title: "Pakiet Formalności zgłoszenia i pozwolenia na budowę",
+      vat: "VAT 23%",
+      options: [
+        { id: 'formal-1', name: "Zgłoszenie Budynku Rekreacji Indywidualnej", price: 4420 },
+        { id: 'formal-2', name: "Zgłoszenie budynku Mieszkalnego lub Pozwolenie na budowę", price: 15640 },
+        { id: 'formal-3', name: "Formalność wykonam we własnym zakresie, kupuję tylko projekt", price: 8113 }
+      ]
+    },
+    {
+      category: 'foundation',
+      title: "Fundamenty",
+      options: [
+        { id: 'foundation', name: "Ocieplona, zbrojona płyta fundamentowa", price: 109000 }
+      ]
+    },
+    {
+      category: 'utilities',
+      title: "Instalacje Sanitarne i Elektryczne",
+      options: [
+        { id: 'utilities', name: "Pakiet Instalacje Sanitarne i Elektryczne", price: 72000 }
+      ]
+    },
+    {
+      category: 'roofing',
+      title: "Ściany Wewnętrzne + Obróbki Blacharskie",
+      options: [
+        { id: 'roofing', name: "Pakiet Obróbki Blacharskie, Ocieplenie Sufitu, Płyty G-K Sufit", price: 85200 }
+      ]
+    },
+    {
+      category: 'heating',
+      title: "Ogrzewanie Budynku",
+      options: [
+        { id: 'heating-1', name: "Folie grzewcze + podłogówka (styropian, paroizolacja, ogrzewanie podłogowe, rozdzielacze, wylewka, CO, CWU)", price: 0, customPrice: true },
+        { id: 'heating-2', name: "Kocioł na paliwo stałe (pelet) + komin + podłogówka", price: 0, customPrice: true },
+        { id: 'heating-3', name: "Pompa ciepła powietrze-woda + podłogówka", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'sanitary',
+      title: "Pakiet Sanitarny",
+      options: [
+        { id: 'sanitary', name: "Szambo 10 m³ lub oczyszczalnia", price: 16500 }
+      ]
+    },
+    {
+      category: 'photovoltaic',
+      title: "Fotowoltaika",
+      options: [
+        { id: 'photovoltaic', name: "Indywidualna wycena uzależniona od mocy systemu", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'terrace',
+      title: "Taras i Oświetlenie",
+      options: [
+        { id: 'terrace', name: "Pakiet Taras i Oświetlenie Zewnętrzne - indywidualna wycena", price: 0, customPrice: true }
+      ]
+    },
+    {
+      category: 'turnkey',
+      title: "Wykończenie pod klucz",
+      options: [
+        { id: 'turnkey', name: "Kompletne wykończenie pod klucz - indywidualne kalkulacje", price: 0, customPrice: true }
+      ]
+    }
+  ]
+};
